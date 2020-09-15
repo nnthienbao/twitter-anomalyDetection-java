18701654a1c275b192c081f395fa19bb54c35723
baonnt <baonnt@vng.com.vn>
2020-09-15 14:40:28 +0700

    update result test unstable

--- src/test/java/com/github/ruananswer/anomaly/JAnomTest.java
+++ src/test/java/com/github/ruananswer/anomaly/JAnomTest.java
@@ -19,7 +19,7 @@ public class JAnomTest {
 
 	@Test
 	public void test() throws IOException {
-		String folderPath = "src/test/resources/data/normal";
+		String folderPath = "src/test/resources/data/unstable";
 		File folder = new File(folderPath);
 //		System.out.println("folder=" + Arrays.toString(folder.list()));
 		String[] list = folder.list();
